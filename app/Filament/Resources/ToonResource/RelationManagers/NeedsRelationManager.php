558c871c96a865bf19ce6864ea8d259637471ece
Scott Webster Wood <treii28@gmail.com>
2024-10-03 13:01:16 -0400

    adding misc files

---
+++ app/Filament/Resources/ToonResource/RelationManagers/NeedsRelationManager.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Filament\Resources\ToonResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class NeedsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'needs';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('Toon Needs')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('Toon Needs')
+            ->columns([
+                Tables\Columns\TextColumn::make('Toon Needs'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
